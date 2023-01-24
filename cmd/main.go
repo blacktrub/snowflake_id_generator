a7b32b3ebfcd50989a1f4138780326d7b39a76a9
blacktrub <lightlocdog@gmail.com>
2023-01-24 20:10:26 +0300

    add snowflake pkg

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"net/http"
+	"snowflake_id_generator/internal/snowflake"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	r := gin.Default()
 	r.GET("id", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"status": "ok"})
+		sn := snowflake.Snowflake{}
+		ctx.JSON(http.StatusOK, gin.H{"id": sn.Get()})
 	})
 
 	r.Run()
---
+++ internal/snowflake/snowflake.go
@@ -0,0 +1,7 @@
+package snowflake
+
+type Snowflake struct{}
+
+func (s Snowflake) Get() int64 {
+	return 1
+}
